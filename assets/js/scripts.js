fa13b7c528307005a386d9d2d93560d2a4395dad
Roque <erikroquesud@hotmail.com>
2022-05-13 12:09:59 -0300

    atualizando as cores dos botões e colocando contador regressivo até zero

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,15 @@
-console.log("hello word");
+let currentNumberWrapper = document.getElementById("currentNumber");
+let currentNumber = 0;
+
+function increment(){
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function decrement(){
+    if(currentNumber>=1){
+        currentNumber = currentNumber - 1;
+        currentNumberWrapper.innerHTML = currentNumber;
+    }
+    
+}
